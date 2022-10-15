638c1541d3a0bbcb6777e35cf6072e166997bd31
EgorLaptev <egor.lapteffff@gmail.com>
2022-10-15 20:41:05 +0300

    Removed vue-yandex-maps package

--- src/main.js
+++ src/main.js
@@ -3,15 +3,5 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import YmapPlugin from 'vue-yandex-maps';
 
-const settings = {
-  apiKey: '',
-  lang: 'ru_RU',
-  coordorder: 'latlong',
-  enterprise: false,
-  version: '2.1'
-}
-
-
-createApp(App).use(store).use(router).use(YmapPlugin, settings).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
