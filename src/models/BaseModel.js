07cb0b889abd4ff185c3f998fe992aef3c4941e3
EgorLaptev <egor.lapteffff@gmail.com>
2022-10-16 11:48:18 +0300

    Added BaseModel

---
+++ src/models/BaseModel.js
@@ -0,0 +1,25 @@
+class BaseModel {
+
+    baseUrl = 'https://turist-ru.herokuapp.com/api/'
+
+    request(route, options) {
+        return new Promise((resolve, reject) => {
+
+            const method = options.method ?? 'GET';
+
+            const headers = options.headers ?? new Headers({
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            })
+
+            if (options.body) options.body = JSON.stringify(options.body)
+
+            fetch(route, options)
+                .then(resp => resp.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err))
+
+        })
+    }
+}
